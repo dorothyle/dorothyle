00a4a59c4b543d4f3a8930414acb3550993803d8
dorothyle <46880658+dorothyle@users.noreply.github.com>
2023-11-14 02:02:03 -0800

    complete html for expertise and tech stack splurge

--- src/Landing.js
+++ src/Landing.js
@@ -17,7 +17,11 @@ const Landing = () => {
           <img src={circlePic} className={style.circlePic} />
           <div className={style.greetingContainer}>
             <h1 className={style.greeting}>Hi, I'm Dorothy</h1>
-            <h1 className={style.greetingDesktop}>Hi,<br/>I'm Dorothy</h1>
+            <h1 className={style.greetingDesktop}>
+              Hi,
+              <br />
+              I'm Dorothy
+            </h1>
             <h2 className={style.description}>
               I am{" "}
               <span className={style.descriptionChanging}>a developer</span>
@@ -31,6 +35,33 @@ const Landing = () => {
         <div className={style.learnContainer}>
           <p className={style.learnMore}>Learn more about me!</p>
           <img className={style.arrowDown} src={arrowDown} />
+        </div>
+
+        <div className={style.expertiseTechContainer}>
+          <div className={style.expertiseContainer}>
+            <h2 className={style.expertiseText}>
+              <span className={style.expertiseBraces}>{"{ "}</span> Expertise
+              <span className={style.expertiseBraces}>{" }"}</span>
+            </h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </div>
+          <div className={style.techStackContainer}>
+            <h2 className={style.techStackText}>
+              <span className={style.techStackBraces}>{"{ "}</span> Tech Stack
+              <span className={style.techStackBraces}>{" }"}</span>
+            </h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </div>
         </div>
       </div>
     </section>
