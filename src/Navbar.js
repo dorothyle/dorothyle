5f9d68eabe1a0d0b39186ee355cf531ad943a7fd
dorothyle <46880658+dorothyle@users.noreply.github.com>
2023-11-16 16:52:02 -0800

    implement expandable navbar for mobile

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,24 +1,33 @@
+import style from "./Navbar.module.css";
 import homeLogo from "./homeLogo.svg";
 import hamburgerMenu from "./hamburgerMenu.svg";
+import { useState } from "react";
 
-const Hamburger = () => {
+const Navbar = () => {
+  const [hamburgerOpen, setHamburgerOpen] = useState(false);
+
+  const toggleHamburger = () => {
+    setHamburgerOpen(!hamburgerOpen);
+  };
+
   return (
     <div className={style.navbarContainer}>
       <img className={style.homeButton} src={homeLogo} />
 
-      <div className={style.navLinksContainer}>
-        <ul>
+      <div className={style.hamburger} onClick={toggleHamburger}>
+        <img src={hamburgerMenu} />
+      </div>
+
+      <div className={style.navlinksContainer}>
+        <ul className={`${style.navlinks} ${hamburgerOpen ? style.open : ""}`}>
           <li>About</li>
           <li>Projects</li>
           <li>Experience</li>
           <li>Resume</li>
         </ul>
-        <div className={style.hamburger}>
-          <img src={hamburgerMenu} />
-        </div>
       </div>
     </div>
   );
 };
 
-export { Hamburger };
+export { Navbar };
