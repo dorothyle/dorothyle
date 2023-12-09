febbe82131554a170d974b4f0c0413f610aedc2c
dorothyle <46880658+dorothyle@users.noreply.github.com>
2023-12-09 00:02:56 -0800

    complete navbar desktop ui

--- src/Navbar.js
+++ src/Navbar.js
@@ -14,24 +14,40 @@ const Navbar = () => {
   return (
     <div>
       <div className={style.navbarContainer}>
-        <img className={style.homeButton} src={homeLogo} />
+        <div className={style.navbarIcons}>
+          <img className={style.homeButton} src={homeLogo} />
 
-        <div className={style.hamburger} onClick={toggleHamburger}>
-          <img className={`${style.hamburgerLogo} ${hamburgerOpen ? style.disappear : ""}`} src={hamburgerMenu} />
-          <img className={`${style.xLogo} ${hamburgerOpen ? style.appear : ""}`} src={xLogo} />
+          <div className={style.hamburger} onClick={toggleHamburger}>
+            <img
+              className={`${style.hamburgerLogo} ${
+                hamburgerOpen ? style.disappear : ""
+              }`}
+              src={hamburgerMenu}
+            />
+            <img
+              className={`${style.xLogo} ${hamburgerOpen ? style.appear : ""}`}
+              src={xLogo}
+            />
+          </div>
+        </div>
+
+        <div className={style.navlinksContainer}>
+          <ul
+            className={`${style.navlinks} ${hamburgerOpen ? style.open : ""}`}
+          >
+            <li>About</li>
+            <li>Projects</li>
+            <li>Experience</li>
+            <li>Resume</li>
+          </ul>
         </div>
       </div>
-      
-      <div className={style.navlinksContainer}>
-        <ul className={`${style.navlinks} ${hamburgerOpen ? style.open : ""}`}>
-          <li>About</li>
-          <li>Projects</li>
-          <li>Experience</li>
-          <li>Resume</li>
-        </ul>
-      </div>
-      <div className={`${style.blurredBackground} ${hamburgerOpen ? style.appear : ""}`}>
-      </div>
+
+      <div
+        className={`${style.blurredBackground} ${
+          hamburgerOpen ? style.appear : ""
+        }`}
+      ></div>
     </div>
   );
 };
