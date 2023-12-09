2ff6f21c58e059af8061cd2f7ff7a00bd6ea55a4
dorothyle <46880658+dorothyle@users.noreply.github.com>
2023-12-08 20:10:29 -0800

    show 'X' symbol when navbar expanded

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,6 +1,7 @@
 import style from "./Navbar.module.css";
 import homeLogo from "./homeLogo.svg";
 import hamburgerMenu from "./hamburgerMenu.svg";
+import xLogo from "./xLogo.svg";
 import { useState } from "react";
 
 const Navbar = () => {
@@ -16,7 +17,8 @@ const Navbar = () => {
         <img className={style.homeButton} src={homeLogo} />
 
         <div className={style.hamburger} onClick={toggleHamburger}>
-          <img src={hamburgerMenu} />
+          <img className={`${style.hamburgerLogo} ${hamburgerOpen ? style.disappear : ""}`} src={hamburgerMenu} />
+          <img className={`${style.xLogo} ${hamburgerOpen ? style.appear : ""}`} src={xLogo} />
         </div>
       </div>
       <div className={style.navlinksContainer}>
