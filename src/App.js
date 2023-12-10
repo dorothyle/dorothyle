9e4d76c12062bd78584efa1779e9615bdb015bbe
dorothyle <46880658+dorothyle@users.noreply.github.com>
2023-12-10 15:48:08 -0800

    add react routers to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import './App.css';
-import { Landing } from './Landing';
+import "./App.css";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Landing } from "./Landing";
+import { Navbar } from "./Navbar";
 
 function App() {
   return (
     <div className="App">
-      <Landing/>
+      <BrowserRouter>
+        <div>
+          <Navbar />
+          <Routes>
+            <Route exact path="/" element={<Landing/>} />
+            <Route path="*" element={<Landing/>} />
+          </Routes>
+        </div>
+      </BrowserRouter>{" "}
     </div>
   );
 }
